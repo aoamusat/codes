28247fb113ca45dd32f2a7b84c8d50bf47d860f0
gbvsilva <graco.babeuf18@gmail.com>
2022-04-13 21:57:07 -0300

    Added count_imbalance.py

---
+++ py/count_imbalance.py
@@ -0,0 +1,18 @@
+
+def countImbalance(weights, k):
+    n = len(weights)
+    count = 0
+    for i in range(n):
+        min_weight = weights[i]
+        max_weight = weights[i]
+        for j in range(i, n):
+            min_weight = min(weights[j], min_weight)
+            max_weight = max(weights[j], max_weight)
+            if max_weight - min_weight <= k:
+                count += 1
+
+    return count
+
+weights = [1, 2, 3, 4]
+k = 3
+print(countImbalance(weights, k))
