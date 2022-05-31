85f8f6a8d14662408f78d486e562dc24f52fd69e
gbvsilva <graco.babeuf18@gmail.com>
2022-05-31 17:28:49 -0300

    Update power_bi.py

--- py/power_bi.py
+++ py/power_bi.py
@@ -3,6 +3,38 @@ from pypodio2 import api
 import os
 from dotenv import load_dotenv
 
+def get_podio_field_values(field):
+	value = ''
+	if field['type'] == "contact":
+		# Nesse caso o campo é multivalorado, então concatena-se com um pipe '|'
+		# Podem haver aspas duplas inseridas no valor do campo. Substituir com aspas simples
+		for elem in field['values']:
+			value += elem['value']['name'].replace("\"", "'") + "|"
+		value = value[:-1]
+	elif field['type'] == "category":
+		value += field['values'][0]['value']['text'].replace("\"", "'")
+	elif field['type'] == "date" or (field['type'] == "calculation" and 'start' in \
+		field['values'][0]):
+		value += field['values'][0]['start']
+	elif field['type'] == "money":
+		value += field['values'][0]['currency'] + " " + field['values'][0]['value']
+	elif field['type'] == "image":
+		value += field['values'][0]['value']['link']
+	elif field['type'] == "embed":
+		value += field['values'][0]['embed']['url']
+	elif field['type'] == "app":
+		# Nesse caso o campo é multivalorado, então concatena-se com um pipe '|'
+		for val in field['values']:
+			value += val['value']['title'].replace("\"", "'") + "|"
+		value = value[:-1]
+	elif field['type'] == "number":
+		value += str(int(float(field['values'][0]['value'])))
+	else:
+		value += str(field['values'][0]['value']).replace("\"", "'")
+
+	return value
+
+
 load_dotenv()
 
 client_id = os.getenv('PODIO_CLIENT_ID')
@@ -12,10 +44,10 @@ password = os.getenv('PODIO_PASSWORD')
 
 podio = api.OAuthClient(client_id,client_secret,username,password)
 
-task_board_app_id = 27017268
-ajustes_app_id = 26033392
+task_board_appID = 27017268
+ajustes_appID = 26033392
 
-app_info = podio.Application.find(ajustes_app_id)
+app_info = podio.Application.find(task_board_appID)
 column_labels = []
 for field in app_info.get('fields'):
     if field['status'] == "active":
@@ -38,38 +70,13 @@ for step in range(0, num_of_items, 250):
 		fields = [x for x in item['fields'] if f"{x['external_id'][:40]}" in column_labels]
 		# Fazendo a comparação entre os campos existentes e os preenchidos
         # Caso o campo esteja em branco no Podio, preencher com '?'
-		j = 0
-		for i in range(len(column_labels)):
+		item_iter = 0
+		for app_iter in range(len(column_labels)):
 			col_value = ""
-			if j < len(fields) and str(fields[j]['external_id'][:40]) == column_labels[i]:
+			if item_iter < len(fields) and str(fields[item_iter]['external_id'][:40]) == column_labels[app_iter]:
 				# De acordo com o tipo do campo há uma determinada forma de recuperar esse dado
-				if fields[j]['type'] == "contact":
-					# Nesse caso o campo é multivalorado, então concatena-se com um pipe '|'
-					# Podem haver aspas duplas inseridas no valor do campo. Substituir com aspas simples
-					for elem in fields[j]['values']:
-						col_value += elem['value']['name'].replace("\"", "'") + "|"
-					col_value = col_value[:-1]
-				elif fields[j]['type'] == "category":
-					col_value += fields[j]['values'][0]['value']['text'].replace("\"", "'")
-				elif fields[j]['type'] == "date" or (fields[j]['type'] == "calculation" and 'start' in \
-					fields[j]['values'][0]):
-					col_value += fields[j]['values'][0]['start']
-				elif fields[j]['type'] == "money":
-					col_value += fields[j]['values'][0]['currency'] + " " + fields[j]['values'][0]['value']
-				elif fields[j]['type'] == "image":
-					col_value += fields[j]['values'][0]['value']['link']
-				elif fields[j]['type'] == "embed":
-					col_value += fields[j]['values'][0]['embed']['url']
-				elif fields[j]['type'] == "app":
-					# Nesse caso o campo é multivalorado, então concatena-se com um pipe '|'
-					for val in fields[j]['values']:
-						col_value += val['value']['title'].replace("\"", "'") + "|"
-					col_value = col_value[:-1]
-				elif fields[j]['type'] == "number":
-					col_value += str(int(float(fields[j]['values'][0]['value'])))
-				else:
-					col_value += str(fields[j]['values'][0]['value']).replace("\"", "'")
-				j += 1
+				col_value = get_podio_field_values(fields[item_iter])
+				item_iter += 1
 			row.append(col_value)
 		datatable.append(row)
 
